3f983827a948e37ac2363d7f8671ed50ac3ee82a
Francisco Paniagua <fpaniagua@larrainvial.com>
2021-02-08 12:45:55 -0300

    Eliminate breaks

--- update_data_ciq.py
+++ update_data_ciq.py
@@ -148,9 +148,7 @@ for i, isin in enumerate(companies['ISIN']):
         ciq_log["Time Last Update"] = end_time_load - start_time_load
         ciq_log["Time CIQ request"] = end_time_request - end_time_load
         ciq_log["Time Dump"] = end_time_dump- end_time_request
-        logs[id_q] = {"CIQ" : {"quarter" : ciq_log, "properties": properties}, "fields": {}}
-        if id_q == 10:
-            break                        
+        logs[id_q] = {"CIQ" : {"quarter" : ciq_log, "properties": properties}, "fields": {}}              
 
 
 def create_key(company, currency, field):
@@ -236,8 +234,6 @@ for id_q, company in companies.iterrows():
         pkl.dump(id_q, file)   
     with open(dbpath + 'temp/update_logs.json', 'w') as file:
         json.dump(logs, file)   
-    if id_q == 10:
-        break    
 
   
 
