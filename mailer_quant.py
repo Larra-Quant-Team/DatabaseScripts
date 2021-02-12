be4d371abf28ddce8270f2841f6e23fc91a66d4c
Francisco Paniagua <fpaniagua@larrainvial.com>
2021-02-12 12:09:22 -0300

    :art: Fix typo

--- mailer_quant.py
+++ mailer_quant.py
@@ -52,10 +52,10 @@ class Mailer():
         self.text = message.as_string()
         return
     
-    def send_message(self, reciber):    
+    def send_message(self, reciver):    
         # Log in to server using secure context and send email
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
             server.login(self.sender_email, self.password)
-            server.sendmail(self.sender_email, reciber, self.text)    
+            server.sendmail(self.sender_email, reciver, self.text)    
             
